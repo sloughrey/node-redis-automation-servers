c2e0024f5f6d93a791ddd3ff208e1781bb481065
Sean Loughrey <s.a.loughrey@gmail.com>
2024-12-09 12:14:29 -0500

    cleanup

--- server1.ts
+++ server1.ts
@@ -1,4 +1,3 @@
-import Redis from "ioredis";
 import { server } from "./batchServer";
 
 //await redis.connect();
--- server2.ts
+++ server2.ts
@@ -1,10 +1,6 @@
-import Redis from "ioredis";
-import BatchServer from "./batchServer";
+import { server } from "./batchServer";
 
-
-const redis = new Redis();
 //await redis.connect();
 
 // start the demo
-const server = new BatchServer(redis);
 server.init();
